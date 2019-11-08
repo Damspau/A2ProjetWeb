a532d0b8dff4ecfde2abecb971a299b4ea745e6c
Stargat3 <44499481+Stargat3@users.noreply.github.com>
2019-11-08 14:38:48 +0100

    header

--- laravel5/resources/views/default.blade.php
+++ laravel5/resources/views/default.blade.php
@@ -4,14 +4,38 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles/css/fontawesome-all.min.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
    <style> 
+   /*Custom Bootsratp*/
+
+   #navbarSupportedContent{
+       position: relative;
+       right: 0%;
+   }
+
+.btn {
+  background-color: white; 
+  border: none;
+  color: #6684bd;
+  padding: 8px 20px;
+  text-align: center;
+  font-size: 16px;
+}
+.btn:hover{
+    background:grey;
+}
+
 .navbar {
   background-color: #6684bd;
+  font-weight:bold;
+
 }
 .navbar .navbar-brand {
   color: #ecf0f1;
@@ -31,13 +55,13 @@
   color: #6684bd; 
 }
 .navbar .navbar-nav .nav-link {
-  color: #ecf0f1;
+  color: #white;
   border-radius: .25rem;
   margin: 0 0.25em;
 }
 .navbar .navbar-nav .nav-link:not(.disabled):hover,
 .navbar .navbar-nav .nav-link:not(.disabled):focus {
-  color: #6684bd;
+  color: white;
 }
 .navbar .navbar-nav .nav-item.active .nav-link,
 .navbar .navbar-nav .nav-item.active .nav-link:hover,
@@ -63,7 +87,7 @@
   border-color: #ecf0f1;
 }
 .navbar .navbar-link {
-  color: #ecf0f1;
+  color: white;
 }
 .navbar .navbar-link:hover {
   color: #6684bd;
@@ -145,27 +169,9 @@
 
     <header>
 
-<nav class="navbar navbar-expand-lg navbar-light" >
-  <a class="navbar-brand" href="#"><img class="w-25 p-10" src="https://arras.cesi.fr/wp-content/uploads/sites/9/2019/05/Cesi_Logo_INGENIEUR_RVB-HD-500x296.jpg"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    @yield('header')
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+
 
 
 
--- laravel5/resources/views/welcome.blade.php
+++ laravel5/resources/views/welcome.blade.php
@@ -4,6 +4,43 @@
 Page d'accueil
 @endsection
 
+
+
+@section('header')
+
+
+
+
+<nav class="navbar navbar-expand-lg navbar-light" >
+  <a class="navbar-brand" href="#"><img class="w-25 p-10" src="https://arras.cesi.fr/wp-content/uploads/sites/9/2019/05/Cesi_Logo_INGENIEUR_RVB-HD-500x296.jpg"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link mr-2" href="#">Accueil<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Activités</a>
+      </li>
+            <li class="nav-item">
+        <a class="nav-link" href="#">Boutique</a>
+      </li>
+           </li>
+            <li class="nav-item">
+        <a class="nav-link" href="#">Connection/Inscription</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+    </form>
+  </div>
+</nav>
+@endsection
+
 @section('content')
     <!-- <div class="flex-center position-ref full-height">
         @if (Route::has('login'))
