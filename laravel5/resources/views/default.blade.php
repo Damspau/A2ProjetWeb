849cb14121073b7ccc7d7b52368b55e38c317575
Stargat3 <44499481+Stargat3@users.noreply.github.com>
2019-11-08 10:48:43 +0100

    template

---
+++ laravel5/resources/views/default.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> @yield('title')</title>
+</head>
+
+<body>
+
+    <header>
+
+
+
+
+
+
+
+
+
+    
+
+    </header>
+
+    @yield('content')
+
+    <section>
+
+    </section>
+
+    <aside>
+
+    </aside>
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
