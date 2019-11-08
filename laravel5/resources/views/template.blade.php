d52c08bbdde54eb89da2c721e0f1745b9eadaef2
Degaule65 <44675765+Degaule65@users.noreply.github.com>
2019-11-08 15:43:26 +0100

    frontAwa
    icon

---
+++ laravel5/resources/views/template.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>template</title>
+	
+	
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+
+<link rel="stylesheet" href="styles/css/fontawesome-all.min.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+
+
+	<!-- Styles -->
+	<style>
+		.tab {
+			float: left;
+			border: 1% solid #ccc;
+			background-color: #6684BD;
+			width: 7%;
+			height: 20%;
+			position:fixed;
+
+		}
+		.logo4{
+			color: #4b6584;
+		}
+		.container{
+	 		top: 85%;
+	 		position: fixed;
+
+		}
+		footer{
+background-color: #7D6D5B;
+}
+
+	</style>
+</head>
+
+
+	<div class="tab"><ul style="font-size: 150%" >
+		<div>
+		<a  href="https://twitter.com/cesiingenieurs%20" rel="noreferrer noopener" target="_blank" title="Twitter"><i class="fab fa-twitter logo4"></i></a>
+</div>
+<div>
+		<a href="https://discord.gg/Y32yARN" rel="noreferrer noopener" target="_blank" title="LinkedIn"><i class="fab fa-discord logo4"></i></a>
+</div><div>
+		<a href="http://www.viadeo.com/fr/company/ei-cesi" rel="noreferrer noopener" target="_blank" title="Viadeo"><i class="fab fa-instagram logo4"></i></a>
+</div><div>
+		<a href="https://www.facebook.com/CESIingenieurs/" rel="noreferrer noopener" target="_blank" title="Facebook"><i class="fab fa-facebook logo4"></i></a></div>
+	</ul></div>
+</div>
+<body>
+</body>
+
+<footer class="mt-5 bg-green" >
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3 col-md-3 col-lg-3">
+                <i class=" navbar-brand" href="#"> information légale: </i>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div >
+                    <i>Mention légale</i>
+                </div>
+                <div>
+                    <i>Politique de protection de données</i>
+                </div>
+
+            </div>
+            <div class="col-sm-3 col-md-3 col-lg-3">
+                <i class="navbar-brand"> Contactez-nous</i>
+            </div>
+
+            <div class="col-sm-3 col-md-3 col-lg-3">
+                <i class="navbar-brand"> © CESI 2019</i>
+            </div>
+        </div>
+
+    </div>
+</footer>
+</body>
+</html> 
