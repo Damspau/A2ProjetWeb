ea8fc6a526eaa05096deeeee0e3b21d2745e2348
Stargat3 <44499481+Stargat3@users.noreply.github.com>
2019-11-11 14:50:09 +0100

    good good

--- laravel5/public/js/main.js
+++ laravel5/public/js/main.js
@@ -1,1 +1,16 @@
 //Scripts//
+
+// mobile-->Menu
+
+(function ($) {
+  $(document).ready(function(){
+    var menu = $('ul#navigation');
+    if(menu.length){
+      menu.slicknav({
+        prependTo: ".mobile_menu",
+        closedSymbol: '+',
+        openedSymbol:'-'
+      });
+    };
+  });
+})(jQuery);
