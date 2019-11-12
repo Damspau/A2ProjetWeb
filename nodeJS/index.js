13603030c9a434034ece0e702f30563afea689cd
Damspau <44500326+Damspau@users.noreply.github.com>
2019-11-12 13:34:11 +0100

    NODE

--- nodeJS/index.js
+++ nodeJS/index.js
@@ -1,5 +1,3 @@
-
-
 //express loader
 var express = require('express');
 // server parameters
@@ -9,7 +7,7 @@ var port = 3000;
 var app = express();
 var myRouter = express.Router();
 //driver bodyParser and mysql
-var bodyParser = require('body-parser'); app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
+var bodyParser = require('body-parser');
 var mysql = require('mysql');
 
 var connexion = mysql.createConnection({
@@ -32,31 +30,60 @@ myRouter.route('/data/:userName')
  let sql = "CALL UserGetData ('" + req.params.userName + "')";
  let query = connexion.query(sql, (err, results) => {
    if(err) throw err;
-   res.send(results);
+   res.send(JSON.stringify({"status": 200, "error": null, "response": results}))
  });;
 })
 myRouter.route('/data')
 //POST add a user
 .post(function(req,res){
   let sql = "CALL PostData ('" + req.body.username + "', '" + req.body.password + "', '" + req.body.email + "','" + req.body.location+ "')";
-  let query = connexion.query(sql, (err, results) => {
+  var input = "{\"email\":\"" + req.body.email + "\"}";
+  var responseLogin = false;
+  let sqlverify = "SELECT email FROM utilisateur WHERE email = '"+ req.body.email + "';";
+  let query = connexion.query(sqlverify, (err, results2) => {
     if(err) throw err;
-    res.send(results);
+            if (JSON.stringify(results2[0])!=input)
+            {
+                console.log(JSON.stringify(results2[0]))
+
+                responseLogin = true;
+                let query = connexion.query(sql, (err, results) => {
+                  console.log("hey");
+                    if(err) throw err;
+                    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+
+
+
+                  });;
+
+            }
+            if (responseLogin!=true)
+            {
+
+                res.send(JSON.stringify({"status": 404, "error": "Email arleady exist", "response": ""}));
+            }
+            else {
+
+            }
   });;
+
+
 
 
 })
 //PUT update location of user
 .put(function(req,res){
-  let sql = "CALL PutData ('" + req.body.username + "', '" + req.body.location + "', '" + req.body.passwordAdmin + "')";
+  let sql = "CALL PutData ('" + req.body.username + "', '" + req.body.location + "', '" + req.body.passwordAdmin + "', '" + req.body.parameter + "')";
   let query = connexion.query(sql, (err, results) => {
     if(err) throw err;
-    res.send(results);
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+
   });;
 })
 
 //route for delete and connexion
 myRouter.route('/connexDel/:identifiant/:mdp')
+//connexion
 .get(function(req,res){
   let sql = "CALL UserLogin ('" + req.params.identifiant + "', '" + req.params.mdp + "')";
   var input = "[{\"email\":\"" + req.params.mdp + "\"}]";
@@ -82,7 +109,7 @@ myRouter.route('/connexDel/:identifiant/:mdp')
   });;
 
 })
-
+//delete
 .delete(function(req,res){
    //res.json({message : "GET connexion", methode : req.method, identifiant : req.params.identifiant, mdp : req.params.mdp });
    let sql = "CALL DeleteData ('" + req.params.identifiant + "', '" + req.params.mdp + "')";
@@ -93,8 +120,9 @@ myRouter.route('/connexDel/:identifiant/:mdp')
 })
 
 
-
-
+//bodyParserInit
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 //start using the router
 app.use(myRouter);
 // parse application/x-www-form-urlencoded
