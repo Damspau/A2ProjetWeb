60588f002fdc254156f624b79c02215ca4bd1e29
Stargat3 <44499481+Stargat3@users.noreply.github.com>
2019-11-12 20:31:16 +0100

    now

--- laravel5/resources/views/products.blade.php
+++ laravel5/resources/views/products.blade.php
@@ -16,34 +16,34 @@
 
 @section('content')
 
-<p>
+<!-- <p>
     <h1 style="text-align: center;"><u>Shop</u></h1>
-</p>
+</p> -->
 
 <div class="container products">
 
     @if(session('success'))
 
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
 
     @endif
 
     <div class="row">
 
         @foreach($products as $product)
-        <div class="col-xs-18 col-sm-6 col-md-3">
-            <div class="thumbnail">
-                <img src="{{ $product->photo }}" style="text-align:center;" width="500" height="300">
-                <div class="caption">
-                    <h4>{{ $product->name }}</h4>
-                    <p>{{ str_limit(strtolower($product->description), 50) }}</p>
-                    <p><strong>Price: </strong> {{ $product->price }}$</p>
-                    <p class="btn-holder"><a href="{{ url('add-to-cart/'.$product->id) }}" class="btn btn-warning btn-block text-center" role="button">Add to cart</a> </p>
+            <div class="col-xs-18 col-sm-6 col-md-3">
+                <div class="thumbnail">
+                    <img src="{{ $product->photo }}" width="500" height="300">
+                    <div class="caption">
+                        <h4>{{ $product->name }}</h4>
+                        <p>{{ str_limit(strtolower($product->description), 50) }}</p>
+                        <p><strong>Price: </strong> {{ $product->price }}$</p>
+                        <p class="btn-holder"><a href="{{ url('add-to-cart/'.$product->id) }}" class="btn btn-warning btn-block text-center" role="button">Add to cart</a> </p>
+                    </div>
                 </div>
             </div>
-        </div>
         @endforeach
 
     </div><!-- End row -->
