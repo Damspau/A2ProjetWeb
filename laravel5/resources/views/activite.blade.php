2d48932c6e5454f32d36169941b1003a001b32a7
Damspau <44500326+Damspau@users.noreply.github.com>
2019-11-18 09:17:30 +0100

    r

--- laravel5/resources/views/activite.blade.php
+++ laravel5/resources/views/activite.blade.php
@@ -139,9 +139,6 @@ CESI Activité
   src="https://code.jquery.com/jquery-3.4.1.js"
   integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
   crossorigin="anonymous">
-jQuery(document).ready(function(){
-           alert("fuck")
-       });
 
 
 
